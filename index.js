b76d568dcaf8e02219b05c5df58b835d573ec85c
Hirushi <hirushi_n@epiclanka.net>
2024-03-24 22:27:13 +0530

    Add db configurations

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
+//MONGODB connection
+console.log(process.env.MONGODB_URL);
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connect to Database"))
+  .catch((err) => console.log(err));
 
+//API
 app.get("/", (req, res) => {
   res.send("server is running");
 });
