d635791c1f9b99237b985f920c1d8299d31a1bf4
Hirushi <hirushi_n@epiclanka.net>
2024-08-04 19:09:22 +0530

    Add products to the database

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 const PORT = process.env.PORT || 8090;
 // MONGODB connection
@@ -90,3 +91,23 @@ app.post("/login", async (req, res) => {
 
 //server is running
 app.listen(PORT, () => console.log("server is running at port : " + PORT));
+
+//Product section
+const schemaProduct = mongoose.Schema({
+  name: String,
+  category: String,
+  image: String,
+  price: String,
+  description: String,
+});
+
+const productModel = mongoose.model("products", schemaProduct);
+
+// save product API
+app.post("/saveProduct", async (req, res) => {
+  console.log(req.body);
+
+  const data = await productModel(req.body);
+  const datasave = await data.save();
+  res.send({ message: "New Product added successfully" });
+});
